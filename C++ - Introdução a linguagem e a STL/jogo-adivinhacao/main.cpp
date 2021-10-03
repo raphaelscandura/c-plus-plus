69f31eea603b18df113fbf19dd7592e068a68a62
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-03 15:14:32 -0300

    Testando input output streams

--- C++ - Introdução a linguagem e a STL/jogo-adivinhacao/main.cpp
+++ C++ - Introdução a linguagem e a STL/jogo-adivinhacao/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "************************************" << std::endl;
-    std::cout << "* Bem-vindo ao jogo de adivinhação *" << std::endl;
-    std::cout << "************************************" << std::endl;
+    cout << "************************************" << endl;
+    cout << "* Bem-vindo ao jogo de adivinhação *" << endl;
+    cout << "************************************" << endl;
+
+    int numero_secreto = 14;
+
+    cout << "O número secreto é " << numero_secreto << "!" << endl;
+
+    int chute;
+    cout << "Qual é o valor do número secreto?" << endl;
+    cin >> chute;
+    cout << "O valor do seu chute foi: " << chute << endl;
+
+    if(chute == numero_secreto){
+        cout << "Parabéns! Você acertou o número secreto" << endl;
+    } else if (chute > numero_secreto){
+        cout << "O seu chute foi maior do que o número secreto" << endl;
+    } else {
+        cout << "O seu chute foi menor do que o número secreto" << endl;
+    }
+
+    system("pause");
 }
