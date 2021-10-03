639204be43feaea113a6aa75723e50b205e127da
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-03 14:45:21 -0300

    Curso iniciado

---
+++ C++ - Introdução a linguagem e a STL/jogo-adivinhacao/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "************************************" << std::endl;
+    std::cout << "* Bem-vindo ao jogo de adivinhação *" << std::endl;
+    std::cout << "************************************" << std::endl;
+}
