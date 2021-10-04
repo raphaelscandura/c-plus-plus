1a0fbc3910b69017eb407be101714eb6ddca9d3a
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-04 15:39:55 -0300

    Avançando no jogo da forca

--- C++ - Introdução a linguagem e a STL/jogo-forca/main.cpp
+++ C++ - Introdução a linguagem e a STL/jogo-forca/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <locale.h>
 #include <string>
+#include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "mingnon";
+map<char, bool> ja_acertou;
+vector<char> chutes_errados;
 
 bool letra_existe(char palpite)
 {
@@ -17,34 +21,123 @@ bool letra_existe(char palpite)
     return false;
 }
 
+void imprime_bonequinho(int erros)
+{
+    if (erros == 0)
+    {
+        cout << "|-----|\n|\n|\n|" << endl;
+    }
+    if (erros == 1)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (0-0)\n|\n|" << endl;
+    }
+    if (erros == 2)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (0-0)" << endl;
+        cout << "|      |\n|" << endl;
+    }
+    if (erros == 3)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (0-0)" << endl;
+        cout << "|     \\|\n|" << endl;
+    }
+    if (erros == 4)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (0-0)" << endl;
+        cout << "|     \\|/\n|" << endl;
+    }
+    if (erros == 5)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (0-0)" << endl;
+        cout << "|     \\|/" << endl;
+        cout << "|      /" << endl;
+    }
+    if (erros == 6)
+    {
+        cout << "|-----|" << endl;
+        cout << "|    (X-X)" << endl;
+        cout << "|     \\|/" << endl;
+        cout << "|      /\\" << endl;
+    }
+}
+
+bool nao_ganhou()
+{
+    for (char letra : PALAVRA_SECRETA)
+    {
+        if (!ja_acertou[letra])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool nao_enforcou()
+{
+    return chutes_errados.size() != 6;
+}
+
+void resultado()
+{
+    if (nao_enforcou())
+    {
+        cout << "Parabéns! Você adivinhou a palavra secreta e ganhou o jogo!" << endl;
+    }
+    else
+    {
+        cout << "O bonequinho enforcou! Suas tentativas acabaram e você não adivinhou a palavra secreta, você perdeu o jogo!" << endl;
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     char palpite;
-    bool nao_enforcou = true;
-    bool nao_ganhou = true;
 
     cout << "******************************" << endl;
     cout << "* Bem-vindo ao jogo da forca *" << endl;
     cout << "******************************" << endl;
 
-    while (nao_enforcou && nao_ganhou)
+    while (nao_enforcou() && nao_ganhou())
     {
         cout << "Palavra secreta: ";
-        for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
+        for (char letra : PALAVRA_SECRETA)
         {
-            cout << "_ ";
+            if (ja_acertou[letra])
+            {
+                cout << letra << " ";
+            }
+            else
+            {
+                cout << "_ ";
+            }
+        }
+        cout << "\nChutes errados: ";
+        for (char letra : chutes_errados)
+        {
+            cout << letra << " ";
         }
         cout << "\nDigite uma letra que você acha que a palavra secreta tem: " << endl;
         cin >> palpite;
 
         if (letra_existe(palpite))
         {
-            cout << "A palavra secreta contém a letra " << palpite << endl;
+            cout << "Você acertou! A palavra secreta contém a letra " << palpite << endl;
+            ja_acertou[palpite] = true;
         }
         else
         {
-            cout << "A palavra secreta não contém a letra " << palpite << endl;
+            cout << "Você errou! A palavra secreta não contém a letra " << palpite << endl;
+            chutes_errados.push_back(palpite);
         }
+        imprime_bonequinho(chutes_errados.size());
     }
+    resultado();
+    system("pause");
 }
