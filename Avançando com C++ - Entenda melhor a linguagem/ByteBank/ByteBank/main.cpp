71f4cd9bc92bf3092c4b32e5ab245f4e375063ea
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-08 16:13:40 -0300

    Refatoração final, curso concluído

--- Avançando com C++ - Entenda melhor a linguagem/ByteBank/ByteBank/main.cpp
+++ Avançando com C++ - Entenda melhor a linguagem/ByteBank/ByteBank/main.cpp
@@ -7,8 +7,7 @@ int main()
 {
 	setlocale(LC_ALL, "pt_BR");
 
-	Titular titularPedro("Pedro", "123.456.789-10");
-	Conta contaDoPedro("123456", "0012", titularPedro);
+	Conta contaDoPedro("123456", "0012", Titular("Pedro", "123.456.789-10"));
 
 	std::cout << "Número: " << contaDoPedro.getNumero() << "\nAgência: " << contaDoPedro.getAgencia() << "\nTitular: " << contaDoPedro.getTitular().getNome() 
 		<< "\nCPF do Titular: " << contaDoPedro.getTitular().getCpf() << std::endl;
