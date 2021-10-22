aa98927f33a58d0153a72432c46dbcdf60216303
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-22 10:48:43 -0300

    Testando smart pointers para alocação na Heap

--- Avançando com C++ - Performance e otimização/Leilao/Leilao/main.cpp
+++ Avançando com C++ - Performance e otimização/Leilao/Leilao/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
 #include <locale.h>
+#include <memory>
+#include "Usuario.hpp"
 
 void* operator new(size_t bytes)
 {
 	std::cout << "Alocando " << bytes << " bytes na heap" << std::endl;
 	return malloc(bytes);
+}
+
+void exibeNomeUsuario(std::shared_ptr<Usuario> usuario)
+{
+	std::cout << usuario->getNome() << std::endl;
 }
 
 int main()
@@ -21,7 +28,11 @@ int main()
 	std::string_view primeiraCidade(cidades.c_str(), cidades.find('&') - 1);
 	std::string_view segundaCidade(cidades.c_str() + cidades.find('&') + 2);
 
-	std::cout << "A primeira cidade é: " << primeiraCidade << "\nA segunda cidade é: " << segundaCidade << std::endl;	
+	std::cout << "A primeira cidade é: " << primeiraCidade << "\nA segunda cidade é: " << segundaCidade << std::endl;
+
+	//Testando smart pointers - unique_ptr, shared_ptr
+	std::shared_ptr<Usuario> usuario = std::make_shared<Usuario>(Usuario("Bananinha Gomes"));
+	exibeNomeUsuario(usuario);
 	
 	return 0;
 }
